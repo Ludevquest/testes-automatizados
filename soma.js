d248468e32f61ca99cd5ff007dcab55b3be3c1d7
Ludevquest <ludevprogbr19@gmail.com>
2024-08-17 18:05:44 -0300

    initial commits

---
+++ soma.js
@@ -0,0 +1,18 @@
+function soma(num1, num2) {
+  return num1 + num2
+}
+
+module.exports = soma
+
+// const nome = 'Luiz'
+
+// const usuario = {
+//   nome: 'Luiz',
+//   email: 'luiz@email.com'
+// }
+
+// export default { // só pode ser utilizado uma única vez
+//   soma,
+//   nome,
+//   usuario
+// }
