ecd32b80256ade13c836054973422ee419524ed4
Ludevquest <ludevprogbr19@gmail.com>
2024-08-19 14:43:52 -0300

    chore: implementation of describe in sum tests

--- soma.test.js
+++ soma.test.js
@@ -1,5 +1,11 @@
 const soma = require('./soma')
 
-test('deve verificar o resultado de uma soma', () => {
-  expect(soma(3, 8)).toBe(11)
+describe('Testes de soma', () => {
+  test('deve verificar o resultado de uma soma', () => {
+    expect(soma(3, 8)).toBe(11)
+  })
+
+  test('deve verificar o resultado incorreto de uma soma', () => {
+    expect(soma(3, 8)).not.toBe(12)
+  })
 })
